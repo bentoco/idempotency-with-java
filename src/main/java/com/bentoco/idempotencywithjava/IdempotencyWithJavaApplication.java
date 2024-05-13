25acab3d1ce84e2a7f285b8e72f9c3cbbe58b098
bentoco <bentoco@outlook.com.br>
2024-05-13 07:25:44 -0300

    initial commit

---
+++ src/main/java/com/bentoco/idempotencywithjava/IdempotencyWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bentoco.idempotencywithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotencyWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotencyWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bentoco/idempotencywithjava/IdempotencyWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentoco.idempotencywithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotencyWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
